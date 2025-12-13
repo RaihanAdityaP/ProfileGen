df125e796cf6df9d0d8d225af3b339db635bce92
rendyt <raihanadityap01@gmail.com>
2025-12-13 07:51:58 +0700

    Add Google Search Console verification

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      inject: {
+        data: {
+          googleVerification: '1RyKwW_N9hpbV_WN59CM5o02h-KCWntW2mGRQRHZCUU'
+        }
+      }
+    })
+  ]
 })
